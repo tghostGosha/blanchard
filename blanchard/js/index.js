debb9c4378e833c8567dfc20faae710416cffcc1
gosha trutnev <tghost@mail.ru>
2021-01-17 17:12:29 +0300

    111

--- blanchard/js/index.js
+++ blanchard/js/index.js
@@ -25,37 +25,21 @@ items.forEach((item) => {
 const sliders = document.querySelectorAll('.swiper-container');
 
 sliders.forEach((el) => {
-	let swiper = new Swiper(el, {
-
-		spaceBetween: 10,
-		loop: true,
-		pagination: {
-			el: el.querySelector('.swiper-pagination'),
-			clickable: true,
-		},
-		navigation: {
-			nextEl: el.querySelector('.swiper-button-next'),
-			prevEl: el.querySelector('.swiper-button-prev'),
-		},
-	});
-});
-
-const slider1 = document.querySelector('.swiper-container-project');
-
-let mySwiper1 = new Swiper(slider1, {
-  slideNextClass: 'project-swiper-button-next',
-  slidePrevClass: 'project-swiper-button-prev',
-
-  slideClass: 'swiper-slide-projects',
-	loop: true,
-	pagination: {
-		el: '.pag-2',
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.project-swiper-button-next',
-		prevEl: '.project-swiper-button-prev',
-	},
+  let swiper = new Swiper(el, {
+    delegate: '.tile.swiper-slide:not(.swiper-slide-duplicate) a, *:not(.swiper-slide) > a',
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: el.querySelector('.swiper-pagination'),
+      clickable: true,
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: el.querySelector('.swiper-button-next'),
+      prevEl: el.querySelector('.swiper-button-prev'),
+    },
+  });
 });
 
 const element = document.querySelector('#selectCustom');
@@ -65,18 +49,18 @@ new Choices(element, {
 });
 
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#swiper-slide-item, #swiper-slide-link').addEventListener('click', function() {
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#swiper-slide-item, #swiper-slide-link').addEventListener('click', function () {
     document.querySelector('#popup').classList.toggle('is-active')
   })
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.tabs__btn').forEach(function(tabsBtn) {
-    tabsBtn.addEventListener('click', function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
+    tabsBtn.addEventListener('click', function (event) {
       const path = event.currentTarget.dataset.path
 
-      document.querySelectorAll('.tab-content').forEach(function(tabContent) {
+      document.querySelectorAll('.tab-content').forEach(function (tabContent) {
         tabContent.classList.remove('tab-content-active')
       })
       document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
@@ -84,15 +68,15 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 })
 
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#btn-event').addEventListener('click', function() {
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#btn-event').addEventListener('click', function () {
     document.querySelector('#hidding-list').classList.toggle('is-active')
     let x = document.getElementById("btn-event");
     if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   })
 })
 
@@ -105,3 +89,30 @@ tippy('#myButton2', {
 tippy('#myButton3', {
   content: 'В стремлении повысить качество  ',
 });
+
+var selector = document.querySelector("input[type='tel']")
+var im = new Inputmask("+7 (999) 999-99-99")
+
+im.mask(selector)
+
+new JustValidate('.form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 15
+    },
+    tel: {
+      required: true,
+      function: () => {
+        const phone = selector.inputmask.unmaskedvalue()
+        return Number(phone) && phone.length === 10
+      }
+    },
+  },
+  messages: {
+    name: 'Как вас зовут?',
+    tel: 'Укажите ваш телефон',
+  },
+});
+
